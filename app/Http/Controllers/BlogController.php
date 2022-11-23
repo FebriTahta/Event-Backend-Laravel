f78da68704cfb1a01b3fcd88a1eabc328a73afc5
FebriTahta <febrirtah@gmail.com>
2022-11-23 21:35:39 +0700

    set storage link & image source link

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -122,7 +122,7 @@ class BlogController extends Controller
                     // $mediumthumbnailpath = public_path('news_image/thumbnail/'.$filename3);
                     // $this->createThumbnail($mediumthumbnailpath, 300, 185);
             
-                    $largethumbnailpath = public_path('news_image/'.$filename);
+                    $largethumbnailpath = public_path('news_image/'.'thumb_'.$filename);
                     $this->createThumbnail($largethumbnailpath, 550, 340);
                     
                     $exist_url     = News::where('news_url', $request->news_ur)->first();
