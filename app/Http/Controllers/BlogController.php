df52f494ed5f505dd4f4804f329e830619ac7981
FebriTahta <febrirtah@gmail.com>
2022-11-23 21:21:08 +0700

    set storage link & image source link

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -109,8 +109,9 @@ class BlogController extends Controller
                     // $request->file('news_image')->move('news_image/thumbnail/',$filename2);
                     // $request->file('news_image')->move('news_image/thumbnail/',$filename3);
                     // $request->file('news_image')->move('news_thumbnail/',$filename4);
-                    $request->file('news_image')->storeAs('public/news_thumbnail', $filename);
+                    
                     $request->file('news_image')->storeAs('public/news_image', $filename);
+                    $request->file('news_image')->storeAs('public/news_thumbnail', $filename);
                     // $request->file('news_image')->storeAs('public/news_image/thumbnail', $filename2);
                     // $request->file('news_image')->storeAs('public/news_image/thumbnail', $filename3);
                     
