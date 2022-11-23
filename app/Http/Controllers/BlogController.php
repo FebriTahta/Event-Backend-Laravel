8401f2304b5cdc0b12515868ad0fde461255cb2e
FebriTahta <febrirtah@gmail.com>
2022-11-23 21:00:49 +0700

    set storage link & image source link

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -103,14 +103,14 @@ class BlogController extends Controller
                     $filename    = time().'.'.$request->news_image->getClientOriginalExtension();
                     // $filename2   = 'small_'.time().'.'.$request->news_image->getClientOriginalExtension();
                     // $filename3   = 'medium_'.time().'.'.$request->news_image->getClientOriginalExtension();
-                    $filename4   = 'large_'.time().'.'.$request->news_image->getClientOriginalExtension();
+                    // $filename4   = 'large_'.time().'.'.$request->news_image->getClientOriginalExtension();
 
-                    $request->file('news_image')->move('news_image/',$filename);
+                    // $request->file('news_image')->move('news_image/',$filename);
                     // $request->file('news_image')->move('news_image/thumbnail/',$filename2);
                     // $request->file('news_image')->move('news_image/thumbnail/',$filename3);
-                    $request->file('news_image')->move('news_thumbnail/',$filename4);
+                    // $request->file('news_image')->move('news_thumbnail/',$filename4);
 
-                    // $request->file('news_image')->storeAs('public/news_image', $filename);
+                    $request->file('news_image')->storeAs('public/news_image', $filename);
                     // $request->file('news_image')->storeAs('public/news_image/thumbnail', $filename2);
                     // $request->file('news_image')->storeAs('public/news_image/thumbnail', $filename3);
                     // $request->file('news_image')->storeAs('news_thumbnail', $filename4);
@@ -176,7 +176,7 @@ class BlogController extends Controller
                             'news_desc'     =>$request->news_desc,
                             'news_url'      =>$news_url_new,
                             'news_image'    =>$filename,
-                            'news_thumb'    =>$filename4,
+                            'news_thumb'    =>$filename,
                             'news_slug'     =>$news_slug_new,
                             'tag_id'        =>$request->tag_id,
                         ]
