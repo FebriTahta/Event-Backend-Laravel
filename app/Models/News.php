6ad31c444c0b077fb07e6d1b8abeaab4cf2bfdf9
FebriTahta <febrirtah@gmail.com>
2022-11-29 22:14:08 +0700

    update and create at using accessor

--- app/Models/News.php
+++ app/Models/News.php
@@ -44,7 +44,7 @@ class News extends Model
 
     public function getCreatedAtAttribute($date)
     {
-        return \Carbon\Carbon::parse($date)->format('d m Y');
+        return \Carbon\Carbon::parse($date)->format('d F Y');
     }
 
     public function getUpdatedAtAttribute($date)
