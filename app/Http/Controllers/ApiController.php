beb7fcb7b2597d6f7ebde2d1ccc6bfa6b4772319
FebriTahta <febrirtah@gmail.com>
2022-12-01 08:50:45 +0700

    blog views

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -109,7 +109,6 @@ class ApiController extends Controller
                     ->join('users', 'news.user_id', 'users.id')
                     ->select('news_title','news_url','news_slug','thumbnail',
                     'news_views','news.id as id','news_stat','news.created_at')
-                    
                     ->limit(4)
                     ->get();
 
